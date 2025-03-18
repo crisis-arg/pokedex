02960d277494dc9bc7eb1f2f3a1d6dfe9e0f4256
Argha Sasmal <namespace.arg@gmail.com>
2025-03-18 21:28:57 +0530

    api working

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +9,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var pokeapi =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
+      body: Center(
+        child: ElevatedButton(
+          onPressed: fetchpokemondata,
+          child: Text('Press'),
+        ),
+      ),
     );
+  }
+
+  void fetchpokemondata() {
+    var url = Uri.https(
+      "raw.githubusercontent.com",
+      "/Biuni/PokemonGO-Pokedex/master/pokedex.json",
+    );
+
+    http
+        .get(url)
+        .then(
+          (onValue) => {
+            if (onValue.statusCode == 200) {print(onValue.statusCode)},
+          },
+        );
   }
 }
