4da670227d2fa01e0b17b3c45663a2fa283f98a1
Argha Sasmal <namespace.arg@gmail.com>
2025-03-18 21:14:53 +0530

    added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Pokedex());
 }
 
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Pokedex extends StatelessWidget {
+  const Pokedex({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(),
+      home:HomeScreen(),
     );
   }
 }
