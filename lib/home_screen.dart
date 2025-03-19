f4f394aaeecfdc52a77d6dadfadee6f5bda8355a
Argha Sasmal <namespace.arg@gmail.com>
2025-03-19 10:58:45 +0530

    gridview

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,15 +14,35 @@ class _HomeScreenState extends State<HomeScreen> {
   var pokeapi =
       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
 
+  late List pokedex;
+
+  @override
+  void initState() {
+    if (mounted) {
+      fetchpokemondata();
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
-      body: Center(
-        child: ElevatedButton(
-          onPressed: fetchpokemondata,
-          child: Text('Press'),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1.4,
+              ),
+              itemCount: pokedex.length,
+              itemBuilder: (context, index) {
+                return Card();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -31,12 +53,13 @@ class _HomeScreenState extends State<HomeScreen> {
       "/Biuni/PokemonGO-Pokedex/master/pokedex.json",
     );
 
-    http
-        .get(url)
-        .then(
-          (onValue) => {
-            if (onValue.statusCode == 200) {print(onValue.statusCode)},
-          },
-        );
+    http.get(url).then((onValue) {
+      if (onValue.statusCode == 200) {
+        var decodedjsondata = jsonDecode(onValue.body);
+        // print(decodedjsondata);
+        pokedex = decodedjsondata['pokemon'];
+        print(pokedex[0]['name']);
+      }
+    });
   }
 }
