b14d47d9f2b52a81cc00ef37aa36306319f1adc9
Argha Sasmal <namespace.arg@gmail.com>
2025-03-20 12:21:29 +0530

    front view almost done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -27,106 +27,132 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
       body:
           pokedex.isEmpty
               ? Center(child: CircularProgressIndicator())
               : Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: Column(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Stack(
                   children: [
-                    Expanded(
-                      child: GridView.builder(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          childAspectRatio: 1.4,
-                          crossAxisSpacing: 10, 
-                          mainAxisSpacing: 10, 
-                        ),
-                        itemCount: pokedex.length,
-                        itemBuilder: (context, index) {
-                          var type = pokedex[index]['type'][0];
-                          return Container(
-                            decoration: BoxDecoration(
-                              color: Colors.green,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              ),
-                            ),
-
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                  bottom: -10,
-                                  right: -10,
-                                  child: Image.asset(
-                                    'images/pokeball.png',
-                                    height:
-                                        MediaQuery.of(context).size.height *
-                                        0.13,
-                                    fit: BoxFit.fitHeight,
-                                  ),
-                                ),
-                                Positioned(
-                                  bottom: 3,
-                                  right: 5,
-                                  child: CachedNetworkImage(
-                                    imageUrl: (pokedex[index]['img'] as String)
-                                        .replaceFirst("http", "https"),
-                                    height:
-                                        MediaQuery.of(context).size.height *
-                                        0.1,
-                                    fit: BoxFit.fitHeight,
-                                  ),
-                                ),
-                                Positioned(
-                                  top: 10,
-                                  left: 5,
-                                  child: Text(
-                                    pokedex[index]['name'],
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                    ),
-                                  ),
-                                ),
-                                Positioned(
-                                  top: 35,
-                                  left: 5,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(8),
-                                      ),
-                                      color: Colors.black26,
-                                    ),
-
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(
-                                        left: 8,
-                                        right: 8,
-                                        top: 4,
-                                        bottom: 4,
-                                      ),
-                                      child: Text(
-                                        type.toString(),
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
+                    Positioned(
+                      top: -30,
+                      right: -50,
+                      child: Image.asset(
+                        'images/pokeball.png',
+                        width: width * 0.6,
+                        fit: BoxFit.fitHeight,
                       ),
                     ),
-                    SizedBox(width: 70),
+                    Positioned(
+                      top: 80,
+                      left: 20,
+                      child: Text(
+                        'Pokedex',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                    Positioned.fill(
+                      top: height * 0.15,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: GridView.builder(
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    childAspectRatio: 1.4,
+                                    crossAxisSpacing: 10,
+                                    mainAxisSpacing: 10,
+                                  ),
+                              itemCount: pokedex.length,
+                              itemBuilder: (context, index) {
+                                var type = pokedex[index]['type'][0];
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(20),
+                                    ),
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      Positioned(
+                                        bottom: -10,
+                                        right: -10,
+                                        child: Image.asset(
+                                          'images/pokeball.png',
+                                          height: height * 0.13,
+                                          fit: BoxFit.fitHeight,
+                                        ),
+                                      ),
+                                      Positioned(
+                                        bottom: 3,
+                                        right: 5,
+                                        child: CachedNetworkImage(
+                                          imageUrl: (pokedex[index]['img']
+                                                  as String)
+                                              .replaceFirst("http", "https"),
+                                          height: height * 0.1,
+                                          fit: BoxFit.fitHeight,
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: 10,
+                                        left: 5,
+                                        child: Text(
+                                          pokedex[index]['name'],
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: 35,
+                                        left: 5,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(8),
+                                            ),
+                                            color: Colors.black26,
+                                          ),
+
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                              left: 8,
+                                              right: 8,
+                                              top: 4,
+                                              bottom: 4,
+                                            ),
+                                            child: Text(
+                                              type.toString(),
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          SizedBox(width: 70),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
