fcfb2730d3b811342d549f73a32c76fb801493f3
Argha Sasmal <namespace.arg@gmail.com>
2025-03-20 21:45:32 +0530

    scrolling problem fix

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -58,99 +58,91 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                     ),
-                    Positioned.fill(
-                      top: height * 0.15,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: GridView.builder(
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2,
-                                    childAspectRatio: 1.4,
-                                    crossAxisSpacing: 10,
-                                    mainAxisSpacing: 10,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 150),
+                      child: GridView.builder(
+                        padding: EdgeInsets.zero,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 1.4,
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 10,
+                        ),
+                        itemCount: pokedex.length,
+                        itemBuilder: (context, index) {
+                          var type = pokedex[index]['type'][0];
+                          return Container(
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                            ),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  bottom: -10,
+                                  right: -10,
+                                  child: Image.asset(
+                                    'images/pokeball.png',
+                                    height: height * 0.13,
+                                    fit: BoxFit.fitHeight,
                                   ),
-                              itemCount: pokedex.length,
-                              itemBuilder: (context, index) {
-                                var type = pokedex[index]['type'][0];
-                                return Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(20),
+                                ),
+                                Positioned(
+                                  bottom: 3,
+                                  right: 5,
+                                  child: CachedNetworkImage(
+                                    imageUrl: (pokedex[index]['img'] as String)
+                                        .replaceFirst("http", "https"),
+                                    height: height * 0.1,
+                                    fit: BoxFit.fitHeight,
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 10,
+                                  left: 5,
+                                  child: Text(
+                                    pokedex[index]['name'],
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
                                     ),
                                   ),
-                                  child: Stack(
-                                    children: [
-                                      Positioned(
-                                        bottom: -10,
-                                        right: -10,
-                                        child: Image.asset(
-                                          'images/pokeball.png',
-                                          height: height * 0.13,
-                                          fit: BoxFit.fitHeight,
-                                        ),
+                                ),
+                                Positioned(
+                                  top: 35,
+                                  left: 5,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(8),
                                       ),
-                                      Positioned(
-                                        bottom: 3,
-                                        right: 5,
-                                        child: CachedNetworkImage(
-                                          imageUrl: (pokedex[index]['img']
-                                                  as String)
-                                              .replaceFirst("http", "https"),
-                                          height: height * 0.1,
-                                          fit: BoxFit.fitHeight,
-                                        ),
-                                      ),
-                                      Positioned(
-                                        top: 10,
-                                        left: 5,
-                                        child: Text(
-                                          pokedex[index]['name'],
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 18,
-                                          ),
-                                        ),
-                                      ),
-                                      Positioned(
-                                        top: 35,
-                                        left: 5,
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.all(
-                                              Radius.circular(8),
-                                            ),
-                                            color: Colors.black26,
-                                          ),
+                                      color: Colors.black26,
+                                    ),
 
-                                          child: Padding(
-                                            padding: const EdgeInsets.only(
-                                              left: 8,
-                                              right: 8,
-                                              top: 4,
-                                              bottom: 4,
-                                            ),
-                                            child: Text(
-                                              type.toString(),
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                          ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                        left: 8,
+                                        right: 8,
+                                        top: 4,
+                                        bottom: 4,
+                                      ),
+                                      child: Text(
+                                        type.toString(),
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
                                         ),
                                       ),
-                                    ],
+                                    ),
                                   ),
-                                );
-                              },
+                                ),
+                              ],
                             ),
-                          ),
-                          SizedBox(width: 70),
-                        ],
+                          );
+                        },
                       ),
                     ),
                   ],
